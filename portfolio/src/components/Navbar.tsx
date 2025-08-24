911eb2f37cb0972bb81f3e85767681d216843e03
vansh-chani <vansh.chani.code@gmail.com>
2025-08-25 02:50:37 +0530

    minor style fixes

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -2,11 +2,11 @@ export default function Navbar() {
     return (
     <nav className="navbar bg-[#0A0A0A] fixed bottom-0 w-full border-t-2 border-t-white/30">
       <div className="container max-w-[960px] mx-auto px-4 py-5 flex flex-row justify-between items-center">
-        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">ABOUT</p>
-        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">PROJECTS</p>
-        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">RESUME</p>
-        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">BLOGS</p>
-        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">CONTACT</p>
+        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">ABOUT</p>
+        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">PROJECTS</p>
+        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">RESUME</p>
+        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">BLOGS</p>
+        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">CONTACT</p>
       </div>
     </nav>
   );
