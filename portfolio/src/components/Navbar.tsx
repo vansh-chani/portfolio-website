6172ed7808eade03af16dfb4fbc92f79e25670eb
vansh-chani <vansh.chani.code@gmail.com>
2025-08-24 22:55:02 +0530

    minor style fixes

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 export default function Navbar() {
     return (
     <nav className="navbar bg-[#0A0A0A] fixed bottom-0 w-full border-t-2 border-t-white/30">
-      <div className="container max-w-[930px] mx-auto px-4 py-5 flex flex-row justify-between items-center">
-        <p className="text-white/75 tracking-wide text-[22px] font-medium font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">ABOUT</p>
-        <p className="text-white/75 tracking-wide text-[22px] font-medium font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">PROJECTS</p>
-        <p className="text-white/75 tracking-wide text-[22px] font-medium font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">RESUME</p>
-        <p className="text-white/75 tracking-wide text-[22px] font-medium font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">BLOGS</p>
-        <p className="text-white/75 tracking-wide text-[22px] font-medium font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">CONTACT</p>
+      <div className="container max-w-[960px] mx-auto px-4 py-5 flex flex-row justify-between items-center">
+        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">ABOUT</p>
+        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">PROJECTS</p>
+        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">RESUME</p>
+        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">BLOGS</p>
+        <p className="text-white/75 tracking-wide text-[20px] font-regular font-jetBrains-mono hover:text-white/100 hover:cursor-pointer">CONTACT</p>
       </div>
     </nav>
   );
