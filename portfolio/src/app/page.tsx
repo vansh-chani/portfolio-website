77b46d6e5d8ed49e494cc9b2ebec80686a18a5ce
vansh-chani <vansh.chani.code@gmail.com>
2025-08-25 21:02:11 +0530

    Added Navbar scroll behaviour

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,13 +1,20 @@
+"use client";
+import React, { useState } from "react";
 import Navbar from "@/components/Navbar";
 import HeroText from "@/components/HeroText";
 
 export default function Home() {
+  const [page, setPage] = useState("about");
+
   return (
-    <main className="min-h-screen bg-[#0A0A0A] min-w-screen">
-      <Navbar />
-      <div className="h-[90vh] bg-[linear-gradient(to_bottom,rgba(0,0,0,0.1)_0%,#0A0A0A_90%),linear-gradient(to_bottom,rgba(233,233,233,0.2)_0%,rgba(10,10,10,.2)_100%),radial-gradient(circle_28vh_at_70%_28%,rgba(112,112,112,0.6)_0%,rgba(0,0,0,0.6)_82%,transparent_100%),radial-gradient(circle_70vh_at_37%_55%,#707070_0%,#000000_60%,transparent_100%)] ">
+    <main className="min-h-screen bg-[#0A0A0A]">
+      <div className="h-[calc(100vh-70px)] bg-[linear-gradient(to_bottom,rgba(0,0,0,0.1)_0%,#0A0A0A_90%),linear-gradient(to_bottom,rgba(233,233,233,0.2)_0%,rgba(10,10,10,.2)_100%),radial-gradient(circle_28vh_at_70%_28%,rgba(112,112,112,0.6)_0%,rgba(0,0,0,0.6)_82%,transparent_100%),radial-gradient(circle_70vh_at_37%_55%,#707070_0%,#000000_60%,transparent_100%)] ">
         <HeroText />
       </div>
+      <div className="nav flex flex-row justify-center">
+        <Navbar />
+      </div>
+      <div className="content-reveal min-h-screen"></div>
     </main>
   );
 }
--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -1,13 +1,136 @@
+"use client";
+import { abort } from "process";
+import React, { useState, useEffect } from "react";
+
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [page, setPage] = useState("about");
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    setHeight(window.innerHeight);
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  function handleClick(event: React.MouseEvent<HTMLParagraphElement>) {
+    const targetId = event.currentTarget.id;
+    setPage(targetId);
+  }
+
+  function HandleScrolledClicked(event: React.MouseEvent<HTMLParagraphElement>) {
+    const targetId = event.currentTarget.id;
+    setPage(targetId);
+
+  }
+
+  if (scrolled === 0) {
     return (
-    <nav className="navbar bg-[#0A0A0A] fixed bottom-0 w-full border-t-2 border-t-white/30">
+      <nav className="navbar bg-[#0A0A0A] fixed bottom-0 w-full border-t-2 border-t-white/30">
+        <NavItems />
+      </nav>
+    );
+  }
+  else if (scrolled >= height - 70) {
+    return (
+      <nav className="navbar flex flex-row items-center bg-[#16161680] fixed top-7 w-[734px] max-h-[58px] border-1   border-white/10 rounded-[20px] transition-all duration-300 ease-in-out ">
+        <NavItemsScrolled />
+      </nav>
+    );
+
+  }
+  else if (scrolled > 0) {
+    return (
+      <nav className="navbar bg-[#0a0a0a80] w-full border-t-2 border-t-white/30 border-b-2 border-b-white/0 border-x-2 border-x-white/0">
+        <NavItems />
+      </nav>
+    );
+  }
+
+  function NavItems() {
+    return (
       <div className="container max-w-[960px] mx-auto px-4 py-5 flex flex-row justify-between items-center">
-        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">ABOUT</p>
-        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">PROJECTS</p>
-        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">RESUME</p>
-        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">BLOGS</p>
-        <p className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101">CONTACT</p>
+        <p onClick={handleClick} className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101" id="about">ABOUT</p>
+        <p onClick={handleClick} className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101" id="projects">PROJECTS</p>
+        <p onClick={handleClick} className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101" id="resume">RESUME</p>
+        <p onClick={handleClick} className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101" id="blogs">BLOGS</p>
+        <p onClick={handleClick} className="text-white/70 tracking-wide text-[20px] font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101" id="contact">CONTACT</p>
       </div>
-    </nav>
-  );
+    );
+  }
+  function NavItemsScrolled() {
+    return (
+      <div className="container max-w-[960px] mx-auto px-8 py-5 flex flex-row justify-between items-center">
+        <p onClick={handleClick}
+          style={{
+            backgroundColor: page === "about" ? "#48484880" : "transparent",
+            borderRadius: page === "about" ? "16px" : "0px",
+            border: page === "about" ? "0.5px solid #ffffff20" : "none",
+            color: page === "about" ? "#ffffff" : "#ffffff/70",
+          }}
+          className="text-white/70 tracking-wide text-[16px] py-2 px-6 font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101 transition-all duration-300 ease-in-out"
+          id="about">
+          ABOUT
+        </p>
+        <p onClick={handleClick}
+          style={{
+            backgroundColor: page === "projects" ? "#48484880" : "transparent",
+            borderRadius: page === "projects" ? "16px" : "0px",
+            border: page === "projects" ? "0.5px solid #ffffff20" : "none",
+            color: page === "projects" ? "#ffffff" : "#ffffff/70",
+          }}
+          className="text-white/70 tracking-wide text-[16px] py-2 px-6 font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101 transition-all duration-300 ease-in-out"
+          id="projects">
+          PROJECTS
+        </p>
+        <p onClick={handleClick}
+          style={{
+            backgroundColor: page === "resume" ? "#48484880" : "transparent",
+            borderRadius: page === "resume" ? "16px" : "0px",
+            border: page === "resume" ? "0.5px solid #ffffff20" : "none",
+            color: page === "resume" ? "#ffffff" : "#ffffff/70",
+          }}
+          className="text-white/70 tracking-wide text-[16px] py-2 px-6 font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101 transition-all duration-300 ease-in-out"
+          id="resume">
+          RESUME
+        </p>
+        <p onClick={handleClick}
+          style={{
+            backgroundColor: page === "blogs" ? "#48484880" : "transparent",
+            borderRadius: page === "blogs" ? "16px" : "0px",
+            border: page === "blogs" ? "0.5px solid #ffffff20" : "none",
+            color: page === "blogs" ? "#ffffff" : "#ffffff/70",
+          }}
+          className="text-white/70 tracking-wide text-[16px] py-2 px-6 font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101 transition-all duration-300 ease-in-out"
+          id="blogs">
+          BLOGS
+        </p>
+        <p onClick={handleClick}
+          style={{
+            backgroundColor: page === "contact" ? "#48484880" : "transparent",
+            borderRadius: page === "contact" ? "16px" : "0px",
+            border: page === "contact" ? "0.5px solid #ffffff20" : "none",
+            color: page === "contact" ? "#ffffff" : "#ffffff/70",
+          }}
+          className="text-white/70 tracking-wide text-[16px] py-2 px-6 font-regular font-jetBrains-mono inline-block hover:text-white/100 hover:cursor-pointer hover:scale-101 transition-all duration-300 ease-in-out"
+          id="contact">
+          CONTACT
+        </p>
+      </div>
+    );
+  }
 }
+
